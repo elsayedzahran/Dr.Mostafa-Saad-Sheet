6dd9ea73f5199550d8e5bc6860e4c57448bd5ca0
Elsayed Zahran <elsayedzahran789@gmail.com>
2023-11-13 01:27:12 +0200

    init

---
+++ Div2A/ColorfulStonesSimplifiedEdition.java
@@ -0,0 +1,26 @@
+package Div2A;
+// problim link
+/*
+https://codeforces.com/contest/405/problem/A
+ */
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class GravityFlip {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        int numberOfBoxes = input.nextInt();
+        input.nextLine();
+        int[] boxes = new int[numberOfBoxes];
+        for (int i = 0 ; i < numberOfBoxes ; i++){
+            boxes[i] = input.nextInt();
+        }
+        Arrays.sort(boxes);
+        for (int column : boxes){
+            System.out.print(column + " ");
+        }
+        System.out.println();
+    }
+}
